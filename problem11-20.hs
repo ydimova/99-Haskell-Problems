a4a08d863c39ba30cfef91dd64d88487a5484300
ydimova <ydimova@gmail.com>
2017-08-20 12:40:41 +0200

    problem 16-17

--- problem11-20.hs
+++ problem11-20.hs
@@ -2,7 +2,7 @@ import Data.List
 
 encode :: (Eq a) => [a] -> [(Int,a)] 
 encode [] = []
-encode l = [(length(head (group l)),head(head (group l)))] ++ encode (concat.tail.group$l)
+encode l = [(length(head (group l)),head(head (group l)))] ++ encode            (concat.tail.group$l)
 
 --Problem 11
 --encodeModified :: (Eq a) => [a] -> [[a]] 
@@ -17,12 +17,17 @@ dupli [] = []
 dupli (x:xs) = [x]++[x]++(dupli xs)
 
 --Propblem 15
-relpi :: [a] -> Int -> [a]
-repli [] _ = []
-repli (x:xs) n = (take n (cycle [x]))++ (cop xs n)
+--relpi :: [a] -> Int -> [a]
+--repli [] _ = []
+--repli (x:xs) n = (take n (cycle [x]))++ (repli xs n)
 
 --Problem 16 : drop every n-th element
-myDrop :: [a] -> Int -> [a]
-myDrop [] _ =[]
-myDrop (x:xs) n
-    |
+dropEvery xs n = map fst $ filter ((/=n). snd) $ zip xs (cycle [1..n])
+
+--Problem 17 : split
+split :: [a] -> Int -> ([a],[a])
+split [] _ = ([],[])
+split l@(x:xs) n
+    | n > 0 = (x:zf,zt)
+    | otherwise = ([],l)
+    where (zf,zt) = split xs (n-1)
